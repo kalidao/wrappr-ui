53a54050a787f5089b49209e0ee05d72f62b432e
shiv <shivanshityagi@icloud.com>
2022-08-15 16:44:30 +0000

    add route home

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
-import { Flex, Button } from '@chakra-ui/react'
+import { Flex, Button, Box } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { MdExplore, MdCreate } from 'react-icons/md'
 
@@ -9,7 +9,9 @@ export default function Header() {
   const router = useRouter()
   return (
     <Flex padding="0 10px" alignItems="center" justifyContent="space-between" minH="10vh">
-      <Image src={'/logo.png'} height={60} width={80} alt={`Wrappr logo`} />
+      <Box onClick={() => router.push('/')}>
+        <Image src={'/logo.png'} height={60} width={80} alt={`Wrappr logo`} />
+      </Box>
       <Flex gap="15px">
         <Button
           onClick={() => router.push('/explore')}
