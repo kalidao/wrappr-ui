3b7215c7d38c21106a65bd124c31d4e4a1bc5b69
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-08-18 01:04:02 +0900

    Update deployments.ts
    deployments

--- src/constants/deployments.ts
+++ src/constants/deployments.ts
@@ -3,7 +3,29 @@ type Contracts = {
 }
 
 export const deployments: { [key: number]: Contracts } = {
+  1: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
   4: {
-    factory: '0x3866DEACE92b6eB1f3A2cE38632F530518934584',
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+    delSeries: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
+  },
+  5: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
+  10: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
+  56: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
+  137: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
+  42161: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
+  },
+  42170: {
+    factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
   },
 }
