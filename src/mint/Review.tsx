09f0b0e941a31589caa27e1f6e783bb2417bd7af
shiv <shivanshityagi@icloud.com>
2022-08-25 10:02:39 +0000

    update review

--- src/mint/Review.tsx
+++ src/mint/Review.tsx
@@ -10,17 +10,27 @@ type ReviewProps = {
 const PDFViewer = dynamic(import('./PDFViewer'), { ssr: false })
 
 export default function Review({ setView }: ReviewProps) {
-  const { isConnected } = useAccount()
+  const [checked, setChecked] = useState(false)
+
+  const mint = () => {}
 
   return (
     <Flex flexDirection="column" gap="10px" justifyContent="center" alignItems="flex-start">
       <PDFViewer src={'/legal/DelLLC.pdf'} />
-      <Checkbox colorScheme="brand">I have read and accept the terms of this agreement.</Checkbox>
-      {isConnected && (
-        <Button type="submit" width="100%" colorScheme="brand" variant="solid" borderRadius={'none'}>
-          Confirm Mint
-        </Button>
-      )}
+      <Checkbox colorScheme="brand" onChange={() => setChecked(!checked)}>
+        I have read and accept the terms of this agreement.
+      </Checkbox>
+      <Button
+        type="submit"
+        width="100%"
+        colorScheme="brand"
+        variant="solid"
+        borderRadius={'none'}
+        disabled={!checked}
+        onClick={mint}
+      >
+        {!checked ? 'Please agree to the terms.' : 'Confirm Mint'}
+      </Button>
     </Flex>
   )
 }
