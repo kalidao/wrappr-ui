f91724428b95f472aafe3c19575abef0b1410516
shiv <shivanshityagi@icloud.com>
2022-09-10 17:59:33 +0000

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { isValidURL } from './isValidURL'
 import { uploadFile } from './uploadFile'
 import { uploadJSON } from './uploadJSON'
+import { uploadBlob } from './uploadBlob'
 
-export { isValidURL, uploadFile, uploadJSON }
+export { isValidURL, uploadFile, uploadJSON, uploadBlob }
