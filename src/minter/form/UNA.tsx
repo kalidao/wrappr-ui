e01a3ec8d7a05bcbeeb615e1d54e6a86739d72c0
shiv <shivanshityagi@icloud.com>
2022-09-14 03:24:52 +0000

    fix una bug

--- src/minter/form/UNA.tsx
+++ src/minter/form/UNA.tsx
@@ -66,7 +66,7 @@ export default function UNA({ store, setStore, setView }: Props) {
           htmlFor="name"
           fontSize="sm"
           colorScheme="gray"
-          className="peer-focus:font-medium absolute text-sm duration-300 transform-translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-zinc-600 peer-focus:dark:text-zinc-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-zinc-600 peer-focus:dark:text-zinc-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Name
         </FormLabel>
