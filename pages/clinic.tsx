701c99a4b83c1e3fd4a4d223a64ec24e52365350
Shiv <shivanshityagi@icloud.com>
2022-12-14 20:23:14 +0530

    🥢 nit clinic

--- pages/clinic.tsx
+++ pages/clinic.tsx
@@ -2,16 +2,26 @@ import type { NextPage } from 'next'
 import Layout from '../src/layout'
 import { Widget } from '@typeform/embed-react'
 import { useRouter } from 'next/router'
+import { useThemeStore } from '~/hooks/useThemeStore'
 
 const Clinic: NextPage = () => {
   const router = useRouter()
+  const mode = useThemeStore((state) => state.mode)
+
   return (
     <Layout
       heading="LexDAO Clinic"
       content="Get help managing your Wrappr from legal engineers at LexDAO Clinic"
       back={() => router.push('/')}
     >
-      <Widget id="FEZ9cFhM" className="h-[90vh] w-full bg-black" />
+      <Widget
+        id="FEZ9cFhM"
+        style={{
+          height: '75vh',
+          width: '100%',
+          backgroundColor: mode === 'dark' ? 'black' : 'white',
+        }}
+      />
     </Layout>
   )
 }
