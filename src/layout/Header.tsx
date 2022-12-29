811351f92b6558f2353afe9d6a92b432dfa80446
Shiv <shivanshityagi@icloud.com>
2022-12-29 22:56:38 +0530

    ☀️🌛

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,6 +7,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount, useNetwork } from 'wagmi'
 import * as styles from './styles.css'
 import { ReactNodeNoStrings } from '@kalidao/reality/dist/types/types'
+import ToggleMode from './ToggleMode'
 
 export default function Header() {
   const router = useRouter()
@@ -29,6 +30,7 @@ export default function Header() {
         <Item label="make" src="/create" icon={<IconPencil />} />
         <Item label="find" src={`/${chain ? chain.id : 1}/explore`} icon={<IconGrid />} />
         <Item label="docs" src="https://docs.wrappr.wtf/get-started/what/" icon={<IconBookOpenSolid />} isExternal />
+        <ToggleMode />
       </Box>
       <Stack direction={'horizontal'}>
         {/* <ToggleMode /> */}
