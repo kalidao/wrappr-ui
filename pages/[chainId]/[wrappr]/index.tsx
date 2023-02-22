3a56682a34de46641cd06562b56ec3b52a2a222e
audsssy <stamfordh@hotmail.com>
2023-02-22 14:05:51 -0500

    add logic to explore wrapprs on Q testnet

--- pages/[chainId]/[wrappr]/index.tsx
+++ pages/[chainId]/[wrappr]/index.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/router'
 import { deployments, WRAPPR } from '~/constants'
 import MintWrapprNFT from '~/wrap/MintWrapprNFT'
 import { ethers } from 'ethers'
+import { compileQtestnetWrapprs } from '~/utils/compileQtestnetWrapprs'
 
 const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter()
@@ -18,6 +19,7 @@ const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServ
     contractInterface: WRAPPR,
     chainId: Number(chainId),
   }
+
   const { data: reads, isLoading: isReading } = useContractReads({
     contracts: [
       {
@@ -29,6 +31,45 @@ const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServ
   const { isLoading, error, data } = useQuery(['wrappr', wrappr?.['baseURI']], () =>
     fetchWrapprData(wrappr?.['baseURI']),
   )
+  console.log(data, wrappr)
+  if (Number(chainId) == 35543) {
+    return (
+      <Layout heading="Wrappr" content="Wrap now" back={() => router.push(`/${chainId}/explore`)}>
+        <Box padding="6">
+          <Stack direction={'horizontal'} align="flex-start" justify={'space-between'}>
+            <Stack>
+              {isLoading ? (
+                <Spinner />
+              ) : (
+                <Avatar src={data?.['image']} size="96" label={`Image for ${data?.['name']}`} shape="square" />
+              )}
+              <MintWrapprNFT
+                chainId={Number(chainId)}
+                wrappr={contractAddress ? (contractAddress as string) : ethers.constants.AddressZero}
+                mintFee={wrappr['mintFee']}
+              />
+            </Stack>
+            <Box width="full">
+              <Stack>
+                <Heading>{reads ? reads?.[0] : 'No name found'}</Heading>
+                {/* className="whitespace-pre-line break-normal text-gray-400" */}
+                <Text wordBreak="break-word">{data ? data['description'] : 'No description found'}</Text>
+                <Heading>Traits</Heading>
+                <Stack>
+                  {data &&
+                    data?.['attributes']?.map((trait: TraitType, index: number) => (
+                      <Trait key={index} trait_type={trait['trait_type']} value={trait['value']} isBig={false} />
+                    ))}
+                  {wrappr ? <Trait trait_type={'Admin'} value={wrappr?.['admin']} isBig={false} /> : <Spinner />}
+                  {/* {wrappr ? <Trait trait_type={'Mint Fee'} value={wrappr?.['mintFee']} isBig={true} /> : <Spinner />} */}
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Box>
+      </Layout>
+    )
+  }
 
   // TODO: Add mint fee
   return (
@@ -73,10 +114,22 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const wrappr = context?.params?.wrappr as string
   const chainId = Number(context?.params?.chainId as string)
 
+  console.log(wrappr)
   if (!chainId)
     return {
       notFound: true,
     }
+
+  const { wrapprsLong } = compileQtestnetWrapprs(chainId)
+  if (chainId == 35443) {
+    for (let i = 0; i < wrapprsLong.length; i++) {
+      if (wrapprsLong[i].id == wrappr) {
+        return {
+          props: { wrappr: wrapprsLong[i] },
+        }
+      }
+    }
+  }
 
   const res = await fetch(deployments[chainId]['subgraph'] as string, {
     method: 'POST',
@@ -100,7 +153,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   })
 
   const data = await res.json()
-
   return {
     props: { wrappr: data['data']['wrapprs'][0] },
   }
--- pages/[chainId]/explore.tsx
+++ pages/[chainId]/explore.tsx
@@ -6,6 +6,7 @@ import { WrapprCard } from '~/wrap'
 import { Wrappr } from '~/types/wrappr.types'
 import { deployments } from '~/constants'
 import { useQuery } from '@tanstack/react-query'
+import { compileQtestnetWrapprs } from '~/utils/compileQtestnetWrapprs'
 
 const Explore: NextPage = () => {
   const router = useRouter()
@@ -31,8 +32,32 @@ const Explore: NextPage = () => {
       .then((data) => data?.['data']?.['wrapprs'])
   })
 
-  // TODO: Add chain not supported if subgraph is undefined for chainId
-  if (chainId && deployments[Number(chainId)]['subgraph'] === undefined) {
+  // Q Testnet
+  if (chainId == 35443) {
+    const { wrapprs } = compileQtestnetWrapprs(chainId)
+    console.log(wrapprs)
+    return (
+      <Layout heading="Wrappr" content="ddWrap now" back={() => router.push('/')}>
+        <Box padding="6">
+          <Stack direction={'horizontal'} align="center" justify={'flex-start'} space="8" wrap>
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              wrapprs?.map((wrappr: Wrappr) => (
+                <WrapprCard
+                  key={wrappr['id']}
+                  name={wrappr.name}
+                  id={wrappr.id}
+                  baseURI={wrappr.baseURI}
+                  chainId={chainId ? chainId.toString() : '1'}
+                />
+              ))
+            )}
+          </Stack>
+        </Box>
+      </Layout>
+    )
+  } else if (chainId && deployments[Number(chainId)]['subgraph'] === undefined) {
     return (
       <Layout heading="Wrappr" content="Wrap now" back={() => router.push('/')}>
         <Box display={'flex'} alignItems="center" justifyContent={'center'}>
@@ -41,6 +66,17 @@ const Explore: NextPage = () => {
       </Layout>
     )
   }
+
+  // TODO: Add chain not supported if subgraph is undefined for chainId
+  // if (chainId && deployments[Number(chainId)]['subgraph'] === undefined) {
+  //   return (
+  //     <Layout heading="Wrappr" content="Wrap now" back={() => router.push('/')}>
+  //       <Box display={'flex'} alignItems="center" justifyContent={'center'}>
+  //         <Text>This chain is not yet supported. Please switch to a supported chain.</Text>
+  //       </Box>
+  //     </Layout>
+  //   )
+  // }
 
   return (
     <Layout heading="Explore" content="Explore wrapprs. Wrap anything." back={() => router.push('/explore')}>
