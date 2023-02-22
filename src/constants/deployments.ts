d17d407361158ac537f7f3e363be743d79850eb1
audsssy <stamfordh@hotmail.com>
2023-02-22 14:04:58 -0500

    update contract addresses

--- src/constants/deployments.ts
+++ src/constants/deployments.ts
@@ -3,6 +3,7 @@ interface Contracts {
   factory: string
   deLLC: string
   wyLLC: string
+  miLLC: string
   deUNA: string
   wyUNA: string
   lexCharter: string
@@ -16,6 +17,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '0xee06D1Eb614003f081F2A98F5e6a8135eBa99AF3',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -27,6 +29,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -38,6 +41,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '0xee06D1Eb614003f081F2A98F5e6a8135eBa99AF3',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -49,6 +53,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '0xee06D1Eb614003f081F2A98F5e6a8135eBa99AF3',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -60,6 +65,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '0xee06D1Eb614003f081F2A98F5e6a8135eBa99AF3',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -71,6 +77,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -81,6 +88,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
@@ -92,6 +100,7 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     orCharter: '0x5228eBc680BA2Ac70A8dB9FBF01CEAaC9Dd9b2Ea',
@@ -102,22 +111,48 @@ export const deployments: { [key: number]: Contracts } = {
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
     orCharter: '0x5228eBc680BA2Ac70A8dB9FBF01CEAaC9Dd9b2Ea',
     subgraph: 'https://api.thegraph.com/subgraphs/name/nerderlyne/wrappr-avalanche',
   },
+  // Q
+  // 35441: {
+  //   factory: '',
+  //   deLLC: '',
+  //   wyLLC: '',
+  //   miLLC: '',
+  //   deUNA: '',
+  //   wyUNA: '',
+  //   lexCharter: '',
+  //   orCharter: '',
+  //   subgraph: '',
+  // },
   // Goerli
   5: {
     testnet: true,
     factory: '0xA945f46Ca376B18fB34d809ef4F21f9b58AE4C50',
     deLLC: '0xaC4EF9D6fC426f8fFd188771d9C688890b634462',
     wyLLC: '0x8d18D533047129dF8172feC7931a3933C47645D2',
+    miLLC: '0xee06D1Eb614003f081F2A98F5e6a8135eBa99AF3',
     deUNA: '0xE22ebfbD3e6609A9550a86545E37af7DE1EE688b',
     wyUNA: '0x73Af00b92073D93b47e1077f796A3D6A12F63909',
     lexCharter: '0xa958f8D815a037E8eDe8194DAD70f9A3f3f94041',
     orCharter: '0x5228eBc680BA2Ac70A8dB9FBF01CEAaC9Dd9b2Ea',
     subgraph: 'https://api.thegraph.com/subgraphs/name/nerderlyne/wrappr-goerli',
+  },
+  // Q Testnet
+  35443: {
+    testnet: true,
+    factory: '0xB96b13E38caBF09A79A8b6a427FBB9e09A1aB6b2',
+    deLLC: '0xaE8F59941A169cfA9C55E9213f7c50409973DB01',
+    wyLLC: '0xB18D52C4662b2F9b9a860406ab6aA5D9C79d8b88',
+    miLLC: '0x119Aef39481c2b7e683Fb1015a10EF8C75A5BF94',
+    deUNA: '0x61FDc87e3BFE16874099650041Db3e11B005d69c',
+    wyUNA: '0x1CEa1b21eE8Cb62ea97c54D0454E10b387bd0AA1',
+    lexCharter: '0xa585417062dcB987cBf197F712532cBBcf7f95D5',
+    orCharter: '0xdEe39515DB9366E1b80168190f7FE78bcbD29E5b',
   },
 }
