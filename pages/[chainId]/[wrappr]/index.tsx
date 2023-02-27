fb2c625e4dff5fa4df959add823b5330ea8933c5
audsssy <stamfordh@hotmail.com>
2023-02-27 12:50:09 -0500

    update

--- pages/[chainId]/[wrappr]/index.tsx
+++ pages/[chainId]/[wrappr]/index.tsx
@@ -10,6 +10,7 @@ import { deployments, WRAPPR } from '~/constants'
 import MintWrapprNFT from '~/wrap/MintWrapprNFT'
 import { ethers } from 'ethers'
 import { compileQtestnetWrapprs } from '~/utils/compileQtestnetWrapprs'
+import MintWrapprNFTonQ from '~/wrap/MintWrapprNFTonQ'
 
 const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter()
@@ -31,45 +32,6 @@ const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServ
   const { isLoading, error, data } = useQuery(['wrappr', wrappr?.['baseURI']], () =>
     fetchWrapprData(wrappr?.['baseURI']),
   )
-  console.log(data, wrappr)
-  if (Number(chainId) == 35543) {
-    return (
-      <Layout heading="Wrappr" content="Wrap now" back={() => router.push(`/${chainId}/explore`)}>
-        <Box padding="6">
-          <Stack direction={'horizontal'} align="flex-start" justify={'space-between'}>
-            <Stack>
-              {isLoading ? (
-                <Spinner />
-              ) : (
-                <Avatar src={data?.['image']} size="96" label={`Image for ${data?.['name']}`} shape="square" />
-              )}
-              <MintWrapprNFT
-                chainId={Number(chainId)}
-                wrappr={contractAddress ? (contractAddress as string) : ethers.constants.AddressZero}
-                mintFee={wrappr['mintFee']}
-              />
-            </Stack>
-            <Box width="full">
-              <Stack>
-                <Heading>{reads ? reads?.[0] : 'No name found'}</Heading>
-                {/* className="whitespace-pre-line break-normal text-gray-400" */}
-                <Text wordBreak="break-word">{data ? data['description'] : 'No description found'}</Text>
-                <Heading>Traits</Heading>
-                <Stack>
-                  {data &&
-                    data?.['attributes']?.map((trait: TraitType, index: number) => (
-                      <Trait key={index} trait_type={trait['trait_type']} value={trait['value']} isBig={false} />
-                    ))}
-                  {wrappr ? <Trait trait_type={'Admin'} value={wrappr?.['admin']} isBig={false} /> : <Spinner />}
-                  {/* {wrappr ? <Trait trait_type={'Mint Fee'} value={wrappr?.['mintFee']} isBig={true} /> : <Spinner />} */}
-                </Stack>
-              </Stack>
-            </Box>
-          </Stack>
-        </Box>
-      </Layout>
-    )
-  }
 
   // TODO: Add mint fee
   return (
@@ -82,11 +44,19 @@ const Wrappr: NextPage = ({ wrappr }: InferGetServerSidePropsType<typeof getServ
             ) : (
               <Avatar src={data?.['image']} size="96" label={`Image for ${data?.['name']}`} shape="square" />
             )}
-            <MintWrapprNFT
-              chainId={Number(chainId)}
-              wrappr={contractAddress ? (contractAddress as string) : ethers.constants.AddressZero}
-              mintFee={wrappr['mintFee']}
-            />
+            {Number(chainId) == 35543 ? (
+              <MintWrapprNFT
+                chainId={Number(chainId)}
+                wrappr={contractAddress ? (contractAddress as string) : ethers.constants.AddressZero}
+                mintFee={wrappr['mintFee']}
+              />
+            ) : (
+              <MintWrapprNFTonQ
+                chainId={Number(chainId)}
+                wrappr={contractAddress ? (contractAddress as string) : ethers.constants.AddressZero}
+                mintFee={wrappr['mintFee']}
+              />
+            )}
           </Stack>
           <Box width="full">
             <Stack>
