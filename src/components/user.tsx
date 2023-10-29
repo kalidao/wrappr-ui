7e47d59e9ff1390b5a4f4a2680fe05225a666377
Shivanshi Tyagi <shivanshityagi@icloud.com>
2023-10-29 22:21:02 +0530

    fix user missing import

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,45 +1,45 @@
-import { Address } from "viem"
-import { useEnsName } from "wagmi"
+import { blo } from 'blo'
+import { useMemo } from 'react'
+import { Address, getAddress, isAddress, zeroAddress } from 'viem'
+import { useEnsAvatar, useEnsName } from 'wagmi'
+import { truncateAddress } from '~/utils/address'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { FullAddressCopy } from './full-address-copy'
 
+export const User = ({ address }: { address: Address }) => {
+  const { data: ensName } = useEnsName({
+    address: address,
+    chainId: 1,
+  })
+  const { data: ensAvatar } = useEnsAvatar({
+    name: ensName,
+    chainId: 1,
+  })
 
-export const User = ({
-    address
-}: {
-    address: Address
-}) => {
-    const { data: ensName } = useEnsName({
-        address: address,
-        chainId: 1,
-    })
-    const { data: ensAvatar } = useEnsAvatar({
-        name: ensName,
-        chainId: 1,
-    })
+  const name = useMemo(() => {
+    if (ensName) return ensName
+    if (address && isAddress(address)) return truncateAddress(getAddress(address))
+    return 'User'
+  }, [ensName, address])
 
-    const name = useMemo(() => {
-        if (ensName) return ensName
-        if (address && isAddress(address)) return truncateAddress(getAddress(address))
-        return 'User'
-    }, [ensName, address])
+  const avatar = useMemo(() => {
+    if (ensAvatar) return ensAvatar
+    if (address && isAddress(address)) return blo(address)
+    return blo(zeroAddress)
+  }, [ensAvatar, address])
 
-    const avatar = useMemo(() => {
-        if (ensAvatar) return ensAvatar
-        if (address && isAddress(address)) return blo(address)
-        return blo(zeroAddress)
-    }, [ensAvatar, address])
-
-    return (
-        <div className="flex flex-row items-center space-x-4">
-            <div className="w-12 h-12">
-                <Avatar
-                    fallback={<AvatarFallback />}
-                    image={<AvatarImage src={avatar} />}
-                />
-            </div>
-            <div className="flex flex-col">
-                <h1 className="text-2xl font-bold">{name}</h1>
-                <FullAddressCopy address={address} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-row items-center space-x-4">
+      <div className="w-12 h-12">
+        <Avatar>
+          <AvatarImage src={avatar} />
+          <AvatarFallback>K</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold">{name}</h1>
+        <FullAddressCopy address={address} />
+      </div>
+    </div>
+  )
 }
